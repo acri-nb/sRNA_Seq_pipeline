6ddaa0b3fc368bf8fe9f180c35188b67bf6ebec0
gwajnberg <gabriel.wajnberg@hotmail.com>
2021-03-09 15:45:14 -0400

    Update Step1_preproces.sh

--- Step1_preproces.sh
+++ Step1_preproces.sh
@@ -16,7 +16,7 @@
 ##before this step recommended the use of :
 ##sshfs acri@192.168.0.200:/mnt/user/LTS/backup2/Reference_data/References_genome/ /References_data/References_genome/acri/
 #star for trilink and netflex mapping 
-/home/iarc/bin/STAR/STAR-2.7.0f/bin/Linux_x86_64/STAR   --runThreadN 20   --genomeDir /References_data/References_genome/acri/Homo_sapiens/hg38/STAR/   --readFilesIn Neo-02-3K_trimmed.fq      --outFileNamePrefix Neo-02-3K   --outFilterScoreMinOverLread 0   --outFilterMatchNmin 16   --outFilterMatchNminOverLread 0   --outFilterMismatchNoverLmax 0.05   --alignIntronMax 1
+/home/iarc/bin/STAR/STAR-2.7.0f/bin/Linux_x86_64/STAR   --runThreadN 20   --genomeDir /References_data/References_genome/acri/Homo_sapiens/hg38/STAR/   --readFilesIn Neo-02-3K_trimmed.fq      --outFileNamePrefix Neo-02-3K   --outFilterScoreMinOverLread 0   --outFilterMatchNmin 16   --outFilterMatchNminOverLread 0   --outFilterMismatchNoverLmax 0.05   --alignIntronMax 1  --alignEndsType EndToEnd
 
 #star for comboflex
 /home/iarc/bin/STAR/STAR-2.7.0f/bin/Linux_x86_64/STAR   --runThreadN 20   --genomeDir /References_data/References_genome/acri/Homo_sapiens/hg38/STAR/   --readFilesIn Neo-02-3K_trimmed.fq      --outFileNamePrefix Neo-02-3K   --outFilterScoreMinOverLread 0   --outFilterMatchNmin 15   --outFilterMatchNminOverLread 0.9  --outFilterMismatchNmax 1 --outFilterMismatchNoverLmax 0.3   --alignIntronMax 1
