15640ea6f7b0bac0ef6b95ee87de99094936d3ac
gwajnberg <gabriel.wajnberg@hotmail.com>
2021-05-27 10:21:27 -0300

    Update Step1_preproces.sh

--- Step1_preproces.sh
+++ Step1_preproces.sh
@@ -15,6 +15,7 @@
 ##reference genome in: acri@192.168.0.200:/mnt/user/LTS/backup2/Reference_data/References_genome/
 ##before this step recommended the use of :
 ##sshfs acri@192.168.0.200:/mnt/user/LTS/backup2/Reference_data/References_genome/ /References_data/References_genome/acri/
+#for star 2.7.6 and above use --readFilesCommand zcat  for compressed fastqs
 #star for trilink and netflex mapping 
 /home/iarc/bin/STAR/STAR-2.7.0f/bin/Linux_x86_64/STAR   --runThreadN 20   --genomeDir /References_data/References_genome/acri/Homo_sapiens/hg38/STAR/   --readFilesIn Neo-02-3K_trimmed.fq      --outFileNamePrefix Neo-02-3K   --outFilterScoreMinOverLread 0   --outFilterMatchNmin 16   --outFilterMatchNminOverLread 0   --outFilterMismatchNoverLmax 0.05   --alignIntronMax 1  --alignEndsType EndToEnd
 
